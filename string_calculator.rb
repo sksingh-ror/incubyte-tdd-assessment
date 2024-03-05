d0a348999b5a4ceb0ae481d7beabbee05a002c61
Shailaja Kant Singh <sksingh5996@gmail.com>
2024-03-05 16:44:03 +0530

    final code for whole problem fixed

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,12 +2,38 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    # Split the input string into an array of numbers
-    numbers_array = numbers.split(",")
+    delimiter = extract_delimiter(numbers)
+    numbers_array = extract_numbers(numbers, delimiter)
 
-    # Convert each string element to an integer and sum them up
-    sum = numbers_array.map(&:to_i).reduce(:+)
+    validate_negative_numbers(numbers_array)
 
-    sum
+    # Ignore numbers greater than 1000
+    valid_numbers = numbers_array.select { |num| num <= 1000 }
+
+    valid_numbers.reduce(:+)
+  end
+
+  private
+
+  def self.extract_delimiter(numbers)
+    # Check if a custom delimiter is specified
+    if numbers.start_with?("//")
+      delimiter = numbers[2]
+      numbers = numbers[(numbers.index("\n") + 1)..-1]
+    else
+      delimiter = ","
+    end
+
+    delimiter
+  end
+
+  def self.extract_numbers(numbers, delimiter)
+    # Split the input string into an array of numbers using the specified delimiter and newline
+    numbers.split(/#{delimiter}|\n/).map(&:to_i)
+  end
+
+  def self.validate_negative_numbers(numbers)
+    negative_numbers = numbers.select { |num| num.negative? }
+    raise "Negative numbers not allowed: #{negative_numbers.join(', ')}" unless negative_numbers.empty?
   end
 end
