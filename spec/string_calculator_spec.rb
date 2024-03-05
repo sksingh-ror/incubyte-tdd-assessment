fd201ab1d221da3376e35c9286aa11b1ffc15401
Shailaja Kant Singh <sksingh5996@gmail.com>
2024-03-05 16:06:16 +0530

    adding more condtion into spec file

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -22,6 +22,36 @@ RSpec.describe StringCalculator do
       end
     end
 
+    context 'when given multiple numbers' do
+      it 'returns the sum of all numbers' do
+        expect(StringCalculator.add('1,2,3,4,5')).to eq(15)
+      end
+    end
+
+    context 'when new lines between numbers' do
+      it 'treats new lines as delimiters and returns the sum' do
+        expect(StringCalculator.add("1\n2,3")).to eq(6)
+      end
+    end
+
+    context 'when a custom delimiter is specified' do
+      it 'uses the custom delimiter and returns the sum' do
+        expect(StringCalculator.add("//;\n1;2")).to eq(3)
+      end
+    end
+
+    context 'when given negative numbers' do
+      it 'throws an exception with the negative numbers in the message' do
+        expect { StringCalculator.add('1,-2,-3,4') }.to raise_error("Negative numbers not allowed: -2, -3")
+      end
+    end
+
+    context 'when numbers are greater than 1000' do
+      it 'ignores numbers greater than 1000 and returns the sum' do
+        expect(StringCalculator.add('2,1001,3')).to eq(5)
+      end
+    end
+
     # Add more test cases as needed for different scenarios
 
   end
